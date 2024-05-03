7ba68a97c1882a2019b074534cac28845ccaeddd
Andre Elvan <aelvan@gmail.com>
2024-05-03 20:23:46 +0200

    Changed parameter names to make named parameters better.

--- src/twigextensions/PowerPackTwigExtension.php
+++ src/twigextensions/PowerPackTwigExtension.php
@@ -11,6 +11,8 @@
 namespace spacecatninja\imagerxpowerpack\twigextensions;
 
 use craft\elements\Asset;
+use spacecatninja\imagerx\adapters\ImagerAdapterInterface;
+use spacecatninja\imagerx\models\TransformedImageInterface;
 use spacecatninja\imagerxpowerpack\PowerPack;
 use Twig\Extension\AbstractExtension;
 
@@ -40,21 +42,27 @@ class PowerPackTwigExtension extends AbstractExtension
             new TwigFunction('pppicture', [$this, 'pppicture'], ['is_safe' => ['html']]),
             new TwigFunction('ppimg', [$this, 'ppimg'], ['is_safe' => ['html']]),
             new TwigFunction('ppplaceholder', [$this, 'ppplaceholder'], ['is_safe' => ['html']]),
+            new TwigFunction('pptransform', [$this, 'pptransform']),
         ];
     }
 
-    public function pppicture(array $sources, array $params = [], array $configOverrides = []): Markup
+    public function pppicture(array $sources, array $params = [], array $config = []): Markup
     {
-        return PowerPack::getInstance()->power->createPicture($sources, $params, $configOverrides);
+        return PowerPack::getInstance()->power->createPicture($sources, $params, $config);
     }
     
-    public function ppimg(Asset|string $image, array|string $transform, array $params = [], array $configOverrides = []): Markup
+    public function ppimg(Asset|string $image, array|string $transform, array $params = [], array $config = []): Markup
     {
-        return PowerPack::getInstance()->power->createImg($image, $transform, $params, $configOverrides);
+        return PowerPack::getInstance()->power->createImg($image, $transform, $params, $config);
     }
     
-    public function ppplaceholder(Asset|string $image, string $output='attr', string $type='dominantColor', ?array $configOverrides = null): string
+    public function ppplaceholder(Asset|string $image, string $output='attr', string $type='dominantColor', ?array $config = null): string
     {
-        return PowerPack::getInstance()->power->createPlaceholder($image, $output, $type, $configOverrides);
+        return PowerPack::getInstance()->power->createPlaceholder($image, $output, $type, $config);
+    }
+    
+    public function pptransform(Asset|ImagerAdapterInterface|string|null $image, array|string $transforms, array $defaults = null, array $config = null): array|TransformedImageInterface|Asset|null
+    {
+        return PowerPack::getInstance()->power->transform($image, $transforms, $defaults, $config);
     }
 }
